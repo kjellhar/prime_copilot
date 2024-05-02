f6d8ad3418769be080d7d607b8c5fc5c02507f4d
kjellhar <kjell@lentic.net>
2024-05-02 12:37:24 +0200

    Even faster

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::time::Instant;
 
 fn main() {
-    //const MAX_LIMIT: usize = (u32::MAX as usize)-1;
-    const MAX_LIMIT: usize = 1024;
+    const MAX_LIMIT: usize = (u32::MAX as usize)-1;
+    //const MAX_LIMIT: usize = 100;
  
 
     let start = Instant::now();
@@ -25,18 +25,19 @@ fn sieve_of_eratosthenes(limit: usize) -> Vec<u32> {
     primes[0] = false;
     let mut p = 3;
     while p * p < limit {
-        if primes[(p-1)/2 as usize] {
+        if primes[p>>1 as usize] {
             let mut i = p * p;
             while i < limit {
-                primes[(i-1)/2 as usize] = false;
+                primes[i>>1 as usize] = false;
                 i += p+p;
             }
         }
         p += 2;
     }
 
-    // Must be modified to relfect the fact that even numbers are no longer in the vector
+    //primes.iter().fold(0,|acc, p| if *p {acc +1} else {acc})
     primes.iter().enumerate()
-        .filter_map(|(i, &prime)| if prime { Some(i as u32) } else { None })
+        .filter_map(|(i, &prime)| if prime { Some((i*2 + 1) as u32) } else { None })
         .collect()
+
 }
